0771c7b7dfa950d7362f686a84b8bfce17ba08ea
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2024-10-29 21:08:42 -0300

    Delete links, login con DTO, autenticacion con JWT

--- src/main/java/com/login/demo/service/UserDetailsServiceImp.java
+++ src/main/java/com/login/demo/service/UserDetailsServiceImp.java
@@ -1,14 +1,22 @@
 package com.login.demo.service;
 
 
+import com.login.demo.dto.AuthLoginRequestDTO;
+import com.login.demo.dto.AuthResponseDTO;
 import com.login.demo.models.UserSec;
 import com.login.demo.repository.IUserRepository;
+import com.login.demo.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -19,6 +27,12 @@ public class UserDetailsServiceImp implements UserDetailsService {
 
     @Autowired
     private IUserRepository userRepo;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException {
@@ -51,6 +65,35 @@ public class UserDetailsServiceImp implements UserDetailsService {
                 userSec.isCredentialNotExpired(),
                 userSec.isAccountNotLocked(),
                 authorityList);
+    }
+
+    public AuthResponseDTO loginUser (AuthLoginRequestDTO authLoginRequest){
+
+        //recuperamos nombre de usuario y contraseña
+        String username = authLoginRequest.username();
+        String password = authLoginRequest.password();
+
+        Authentication authentication = this.authenticate (username, password);
+        //si todo sale bien
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String accessToken = jwtUtils.createToken(authentication);
+        AuthResponseDTO authResponseDTO = new AuthResponseDTO(username, "login ok", accessToken, true);
+        return authResponseDTO;
+
+    }
+
+    public Authentication authenticate (String username, String password) {
+        //con esto debo buscar el usuario
+        UserDetails userDetails = this.loadUserByUsername(username);
+
+        if (userDetails==null) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        // si no es igual
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("Invalid password");
+        }
+        return new UsernamePasswordAuthenticationToken(username, userDetails.getPassword(), userDetails.getAuthorities());
     }
 
 }
